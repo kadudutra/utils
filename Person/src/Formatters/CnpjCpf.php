fa381d83d9db5c073b63cf2576c2c0170c34c245
Kadu Dutra <carlos.rocha@edu.unipar.br>
2020-05-11 20:29:51 -0300

    classe de formatação de cnpj e cpf

---
+++ Person/src/Formatters/CnpjCpf.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Person\Formatters;
+
+/**
+ * Utils
+ *
+ * @copyright (c) 2019.
+ * @link <https://github.com/kadudutra/utils.git>
+ * @author Kadu Dutra <kadudutra7@gmail.com>
+ */
+class CnpjCpf
+{
+
+}
