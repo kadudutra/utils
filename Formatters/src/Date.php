b6992eac2c98aba93a836aea3579942c331e05d2
Kadu Dutra <carlos.rocha@edu.unipar.br>
2020-05-15 07:37:42 -0300

    formatação e criação de datas

--- Formatters/src/Date.php
+++ Formatters/src/Date.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Formatters;
 
 use Formatters\Enum\DateFormats;
+use Formatters\Enum\TypesTime;
 
 /**
  * @copyright (c) 2020.
@@ -29,5 +30,83 @@ class Date
     {
         $newDate = \DateTime::createFromFormat($originFormat, $date);
         return $newDate->format($toFormat);
+    }
+
+    /**
+     * Cria uma data atual em um formato especifico
+     *
+     * @param string $responseFormat
+     * @return string
+     */
+    public static function createDateNow($responseFormat = DateFormats::DATE_US): string
+    {
+        $now = (new \DateTime())->format(DateFormats::DATE_BR);
+        return self::convertDate($now, DateFormats::DATE_BR, $responseFormat);
+    }
+
+    /**
+     * Adicionar dias em uma data
+     *
+     * @param string $date
+     * @param int $days
+     * @param string $responseFormat
+     * @return string
+     */
+    public static function addDaysToDate(
+        string $date,
+        int $days,
+        string $responseFormat = DateFormats::DATE_BR
+    ): string
+    {
+        return self::operationTimeToDate($date, $days, 'add', TypesTime::DAY, $responseFormat);
+    }
+
+    /**
+     * Remove dias em uma data
+     *
+     * @param string $date
+     * @param int $days
+     * @param string $responseFormat
+     * @return string
+     */
+    public static function removeDaysToDate(
+        string $date,
+        int $days,
+        string $responseFormat = DateFormats::DATE_BR
+    ): string
+    {
+        return self::operationTimeToDate($date, $days, 'sub', TypesTime::DAY, $responseFormat);
+    }
+
+    /**
+     * @param string $date
+     * @param int $quantity
+     * @param string $typeTime
+     * @param string $operation
+     * @param string $responseFormat
+     * @return string
+     */
+    private static function operationTimeToDate(
+        string $date,
+        int $quantity,
+        string $typeTime,
+        string $operation,
+        string $responseFormat
+    ): string
+    {
+        $date = \DateTime::createFromFormat($responseFormat, $date);
+        $dateInterval = self::dateInterval($quantity, $typeTime);
+        $date->$operation($dateInterval);
+        return $date->format($responseFormat);
+    }
+
+    private static function dateInterval(int $quantity, string $typeTime): \DateInterval
+    {
+        $paramDateInterval = sprintf(
+            "P%s%s",
+            $quantity,
+            $typeTime
+        );
+        return new \DateInterval($paramDateInterval);
     }
 }
---
+++ Formatters/src/Enum/TypesTime.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Formatters\Enum;
+
+class TypesTime
+{
+    public const YEAR   = 'y';
+    public const MONTH  = 'm';
+    public const DAY    = 'd';
+    public const HOUR   = 'h';
+    public const MINUTE = 'i';
+    public const SECOND = 's';
+}
