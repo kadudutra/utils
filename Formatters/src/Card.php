fd5e51b71f3fb96a763664d63f7f46d83a8f2a82
kadudutra <carlos.rocha@edu.unipar.br>
2020-09-03 14:07:06 -0300

    método de mascara ao cartão de crédito

---
+++ Formatters/src/Card.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Formatters;
+
+/**
+ * @copyright (c) 2020.
+ * @link <https://github.com/kadudutra/utils.git>
+ * @author Kadu Dutra <kadudutra7@gmail.com>
+ */
+class Card
+{
+    /**
+     * Adiciona máscara ao cartão de crédito
+     *
+     * @param string $card
+     * @return string
+     */
+    public static function addMask(string $card): string
+    {
+        return substr_replace(trim($card), "******", 6, 6);
+    }
+}
